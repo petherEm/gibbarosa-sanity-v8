e42c34936ec5f5fdee996359efef2c128c27e493
Piotr Maciejewski <pm@mac.home>
2025-04-28 18:31:40 +0200

    plugin eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,18 +13,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    plugins: {
+      "@typescript-eslint": typescriptPlugin
+    },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-non-null-assertion": "off",
       "@typescript-eslint/no-unused-expressions": "off",
       "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/ban-types": ["error", {
-        "types": {
-          "{}": false
-        },
-        "extendDefaults": true
-      }],
       "react-hooks/rules-of-hooks": "off",
       "no-unused-vars": "off",
       "no-unused-expressions": "off",
